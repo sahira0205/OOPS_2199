61f0287d6eb47f0778160508ca45c2ceaee7a7db
sahira0205 <156178749+sahira0205@users.noreply.github.com>
2024-08-31 10:48:35 +0530

    lec1

---
+++ ClassWork/lec.cpp
@@ -0,0 +1,23 @@
+// Add two complex number
+#include <iostream>
+using namespace std;
+class complex{
+   public:
+   	int real;
+   int img;
+   complex add (complex c){
+       complex temp;
+       temp.real=real+c.real;
+       temp.img=img+c.img;
+       return temp;
+   }
+};
+int main() {
+    complex c1,c2,c3;
+    c1.real=5,c1.img=3;
+    c2.real=10,c2.img=2;
+    c3=c1.add(c2);
+    cout<<c3.real<<"+"<<c3.img<<"i"<<endl;
+   
+    return 0;
+}
